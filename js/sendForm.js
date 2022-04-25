6dcbbc49666a690fdea47f73ca9fa039d4f36215
Ve1okot <85491311+Ve1okot@users.noreply.github.com>
2022-04-25 17:05:37 +0300

    fix: day4 add comments

--- js/sendForm.js
+++ js/sendForm.js
@@ -3,19 +3,19 @@ const sendForm = () => {
   const sendButton = document.querySelector("form button.button.modal__button");
 
   form.addEventListener("submit", (event) => {
-    event.preventDefault();
+    event.preventDefault(); // отмена дефолтного действия кнопки
 
     const text = form.querySelector("input[type=text]");
     const tel = form.querySelector("input[type=tel]");
     const email = form.querySelector("input[type=email]");
 
-    const sendObj = {
+    const sendObj = {  // объект со значениями полей формы
       name: text.value,
       phone: tel.value,
       email: email.value,
     };
 
-    fetch("https://jsonplaceholder.typicode.com/posts", {
+    fetch("https://jsonplaceholder.typicode.com/posts", { // фетч с сайта
       method: "POST",
       body: JSON.stringify(sendObj),
       headers: {
@@ -23,22 +23,22 @@ const sendForm = () => {
       },
     })
       .then(response => {
-          if (response.status < 200 && response.status > 210) {
+          if (response.status < 200 && response.status > 210) { // если статус не в диапазоне успешности
               throw new Error('Ошибка!')
           }
           console.log(`Успешно - ${response.status}`);
           return response.json()
       })
       .catch(error => {
-          alert('Ошибка! Данные не отправлены.');
+          alert('Ошибка! Данные не отправлены.');  //  в случае ошибки алерт пользователю
       })
-      .finally(() => {
+      .finally(() => { // наконец-то очистка формы
         text.value = "";
         tel.value = "";
         email.value = "";
-        sendButton.textContent = "Готово!";
+        sendButton.textContent = "Готово!"; // на кнопке меняется надпись
         setTimeout(() => {
-          form.style.display = "";
+          form.style.display = ""; // задержка 1500 мс и закрылие модального окна
         }, 1500);
       });
   });
